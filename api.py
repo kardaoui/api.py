392cdb8fa07d73a254ded2cbada7ceb618353f35
kardaoui <karmehd1@hotmail.com>
2022-06-03 19:19:54 +0200

    premier jet de l'api et liaison avec le dashbord

--- api.py
+++ api.py
@@ -1,26 +1,26 @@
-from flask import Flask, jsonify, request
+import pickle
 
 import pandas as pd
-import numpy as np
-
-import pickle
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET", "POST"])
+
+@app.route('/score', methods=["GET", "POST"])
 def credit():
     data = pd.DataFrame([request.get_json()])
     score = get_score(data)
     return jsonify(score)
 
+@app.before_first_request
 def load_model():
+    global clf
     """loading the trained model"""
-    with open('model/model_test.pkl', 'rb') as mdl:
+    with open('model/model_shap.pkl', 'rb') as mdl:
         clf = pickle.load(mdl)
-    return clf
+
 
 def get_score(data_client):
-    clf = load_model()
     score = clf.predict_proba(data_client)
     keys = (0, 1)
     return dict(zip(keys, score[0]))
